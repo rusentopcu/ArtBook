6a5cffd29b4b21c804d64d004bfa88cbb169a912
Rusen Topcu <rusentopcu@msn.com>
2020-01-28 11:15:24 +0300

    Project create

---
+++ ArtBookver1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtBookver1
+//
+//  Created by Rusen Topcu on 28.01.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
