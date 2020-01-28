d736cd446d44d821b7a7a0eadd0a4ee979871d75
Rusen Topcu <rusentopcu@msn.com>
2020-01-28 12:38:29 +0300

    to design for App

---
+++ ArtBookver1/View Controllers/detailsVC.swift
@@ -0,0 +1,35 @@
+//
+//  detailsVC.swift
+//  ArtBookver1
+//
+//  Created by Rusen Topcu on 28.01.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+        
+    }
+    
+
+}
