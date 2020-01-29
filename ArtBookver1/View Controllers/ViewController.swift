2d2d9501c1b1ad32291063604f656a06f2e079f1
Rusen Topcu <rusentopcu@msn.com>
2020-01-29 14:01:31 +0300

    TableView Settings & Data Fetch create

--- ArtBookver1/View Controllers/ViewController.swift
+++ ArtBookver1/View Controllers/ViewController.swift
@@ -7,21 +7,78 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+ 
+    //MARK: - Arrays for Data Fetch
+    var nameArray = [String]()
+    var idArray = [UUID]()
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         //MARK: - Bar Button Create
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         
+        //MARK: - Data Fetch call func.
+        getData()
     }
     
     //MARK: - Functions
     @objc func addButtonClicked() {
         performSegue(withIdentifier: "todetailsVC", sender: self)
     }
+    
+    
+    func getData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let contex = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try contex.fetch(fetchRequest)
+            for result in results as! [NSManagedObject] {
+                
+                if let name = result.value(forKey: "name") as? String {
+                    self.nameArray.append(name)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                
+                self.tableView.reloadData()
+                
+            }
+            
+        }
+        catch {
+            print("error")
+        }
+        
+    }
+    
+    
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+     }
 
 }
 
