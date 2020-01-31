49fd4ebb6b969d5ca349e48c9c58516212289f0d
Rusen Topcu <rusentopcu@msn.com>
2020-01-31 11:47:00 +0300

    Delete process

--- ArtBookver1/View Controllers/ViewController.swift
+++ ArtBookver1/View Controllers/ViewController.swift
@@ -62,18 +62,23 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         do {
             
             let results = try contex.fetch(fetchRequest)
-            for result in results as! [NSManagedObject] {
+            
+            if results.count > 0 {
                 
-                if let name = result.value(forKey: "name") as? String {
-                    self.nameArray.append(name)
+                    for result in results as! [NSManagedObject] {
+                    
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    
+                    self.tableView.reloadData()
+                    
                 }
-                
-                if let id = result.value(forKey: "id") as? UUID {
-                    self.idArray.append(id)
-                }
-                
-                self.tableView.reloadData()
-                
+
             }
             
         }
@@ -110,6 +115,47 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         selectedPaintingId = idArray[indexPath.row]
         performSegue(withIdentifier: "todetailsVC", sender: nil)
     }
-
+    
+    //MARK: - Delete process
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let contex = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = idArray[indexPath.row].uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                
+                let results = try contex.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == idArray[indexPath.row] {
+                                contex.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                do {
+                                    try contex.save()
+                                }
+                                catch {
+                                    print("error")
+                                }
+                             }
+                          }
+                     }
+                }
+        }
+            catch {
+                print("error")
+        }
+            
+        }
+    }
 }
 
